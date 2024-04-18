806ca5a77273773a4812174d98918d37debeb941
Ekaitz B <iker.sanchezhe@elorrieta-errekamari.com>
2024-04-18 14:16:12 +0200

    erabiltzailea vistetatik hartu eta datu basetik komprobatu

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -7,12 +7,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import model.Aldagaiak;
+import model.db.DB_funtzioak;
+
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
@@ -20,9 +25,9 @@ public class Login extends JFrame {
     private JPanel contentPane;
     private JPanel panel;
     private JLabel lblUsuario;
-    private JTextField textField;
+    private JTextField textFieldBezeroa;
     private JLabel lblContraseña;
-    private JPasswordField passwordField;
+    private JPasswordField passwordFieldPasahitza;
     private JComboBox<String> comboBoxRoles;
     private JButton btnLogin;
     private JButton btnRegistrar;
@@ -55,18 +60,18 @@ public class Login extends JFrame {
         lblUsuario.setBounds(70, 50, 80, 20); // Posición y tamaño del label de usuario
         panel.add(lblUsuario);
 
-        textField = new JTextField();
-        textField.setBounds(160, 50, 150, 20); // Posición y tamaño del campo de texto de usuario
-        panel.add(textField);
+        textFieldBezeroa = new JTextField();
+        textFieldBezeroa.setBounds(160, 50, 150, 20); // Posición y tamaño del campo de texto de usuario
+        panel.add(textFieldBezeroa);
 
         lblContraseña = new JLabel("Contraseña:");
         lblContraseña.setHorizontalAlignment(SwingConstants.RIGHT);
         lblContraseña.setBounds(70, 80, 80, 20); // Posición y tamaño del label de contraseña
         panel.add(lblContraseña);
 
-        passwordField = new JPasswordField();
-        passwordField.setBounds(160, 80, 150, 20); // Posición y tamaño del campo de contraseña
-        panel.add(passwordField);
+        passwordFieldPasahitza = new JPasswordField();
+        passwordFieldPasahitza.setBounds(160, 80, 150, 20); // Posición y tamaño del campo de contraseña
+        panel.add(passwordFieldPasahitza);
 
         comboBoxRoles = new JComboBox<String>();
         comboBoxRoles.addItem("Bezeroa");
@@ -75,10 +80,27 @@ public class Login extends JFrame {
         panel.add(comboBoxRoles);
         
         btnLogin = new JButton("Login");
+        btnLogin.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String bezeroa = textFieldBezeroa.getText();
+        		String pasahitza = passwordFieldPasahitza.getText();
+        		try {
+        			System.out.println(DB_funtzioak.komprobatuErabiltzailea(bezeroa, pasahitza));
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        		
+        	}
+        });
         btnLogin.setBounds(120, 150, 90, 30); // Posición y tamaño del botón Login
         panel.add(btnLogin);
 
         btnRegistrar = new JButton("Registrar");
+        btnRegistrar.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
         btnRegistrar.setBounds(240, 150, 90, 30); // Posición y tamaño del botón Registrar
         panel.add(btnRegistrar);
 	}
