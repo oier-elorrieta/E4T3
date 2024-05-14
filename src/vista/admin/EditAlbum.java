de622dec74b20086b76f1e0b2de8707784da58e8
JonVazquez <jonvazquezfernandez@elorrieta-errekamari.com>
2024-05-14 14:14:09 +0200

    abestia sartu

---
+++ src/vista/admin/EditAlbum.java
@@ -0,0 +1,18 @@
+package vista.admin;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import model.objektuak.Album;
+import model.objektuak.Musikaria;
+
+public class EditAlbum extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public EditAlbum(Album album) {
+		/* jlabels pa modificar losvalues, bioton de atzera y boton de aceptar que cree objeto album llamo al dao pa modificar (falta dao por hacer) */
+	}
+}
+
